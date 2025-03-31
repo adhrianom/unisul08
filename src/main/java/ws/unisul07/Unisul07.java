0161185921a647957a512514c48b1abafc1fb985
feeh <feeh@DESKTOP-2FBFR4I>
2025-03-31 13:15:09 -0300

    ex07

---
+++ src/main/java/ws/unisul07/Unisul07.java
@@ -0,0 +1,11 @@
+
+package ws.unisul07;
+
+import javax.swing.JOptionPane;
+public class Unisul07 {
+
+    public static void main(String[] args) {
+        String name = JOptionPane.showInputDialog("Qual seu nome?");
+        JOptionPane.showMessageDialog(null, name);
+    }
+}
